e83772ce167c97edfea409792416c9eed89c1f12
vineet <jammackp@gmail.com>
2026-06-22 21:42:25 +0530

    d

---
+++ 02-Typescript/@latest/src/apis/Document.ts
@@ -0,0 +1,13 @@
+// AXIOS
+/*
+Powerfull HTTP client for the browser and node.js
+
+- Isomorphic: Runs in the browser and node.js with the same codebase.
+- Promise-based: Uses Promises for handling asynchronous operations, making it easier to work with async code.
+- Intercepting requests and responses: Allows you to intercept and modify requests or responses before they are handled by then or catch.
+- Automatic JSON data transformation: Automatically transforms JSON data when sending requests or receiving responses.
+- Client-side support for protecting against XSRF: Provides built-in support for Cross-Site Request Forgery (XSRF) protection.
+- Abort Controllers: Supports aborting requests using the AbortController API.
+
+*/
+
